c033b4bdcd2ca940d9b3c2fdbce41ce212cac989
Tonmoy Debnath <tonmoy.debnath@northsouth.edu>
2023-10-05 16:27:47 +0600

    update

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require('cors');
+
+const port= process.env.PORT || 5000
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mpylzkg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run(){
+  try{
+        const data=client.db('Tna').collection('data');
+   
+    app.get('/details',async(req,res)=>{
+      const query={};
+      const cursor=data.find(query);
+      const product= await cursor.toArray();
+      res.send(product);
+      console.log(product);
+    });
+    app.post('/infodata',async(req,res)=>{
+      const item=req.body;
+      const result= await data.insertOne(item);
+      res.send(result);
+    });
+    app.get('/products/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)};
+      const item=await allproducts.findOne(query);
+      res.send(item);
+    })
+  }
+  finally{
+
+  }
+}
+run().catch(console.log);
+
+app.get('/',async(req,res)=>{
+  res.send('tna server is running');
+})
+
+app.listen(port,()=> console.log('portal running'));
